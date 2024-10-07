eaf5eb4f2b8731924cf25abfb3f1bb0afdeacead
divyaka777 <divyaka77@gmail.com>
2024-10-07 15:41:11 +0200

    update:chore/Reset

--- cypress/e2e/Tests/Reset.cy.js
+++ cypress/e2e/Tests/Reset.cy.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import HomePage from "../Pages/HomePage";
-import LoginPage from "../Pages/LoginPage";
 
 describe('Near Me Application', () => {
     const homePage = new HomePage();
@@ -20,31 +18,6 @@ describe('Near Me Application', () => {
             cy.get('input[id="email"]').clear().type('divyaka77@gmail.com');
             cy.get('input[id="password"]').clear().type('Newyear@2020');
             homePage.submit();
+        
         });
     });
-=======
-describe('Near Me Application', () => {
-    beforeEach(() => {
-      cy.visit('/'); // Visits the homepage
-    });
-    it('Should verify the reset functionality ', () => {
-        cy.fixture('user').then((user)  =>  { 
-            cy.get('a[href="/login"]').click();
-            cy.get('a[href="/reset"]').click();
-            cy.get('input[id="email"]').clear().type(user.email);
-            cy.get('button[type="submit"].btn').click();
-        });
-    });
-    it('Should display a confirmation message after requesting a password reset', () => {
-        // Load user fixture
-        cy.fixture('user').then((user) => {
-            cy.get('a[href="/login"]').click();
-            cy.get('a[href="/reset"]').click();
-            cy.get('input[id="email"]').clear().type(user.email);
-            cy.get('button[type="submit"]').click();
-          // Assert that the confirmation message is visible
-            cy.contains('Email with password reset request has been sent.').should('be.visible');
-        });
-    });
-});
->>>>>>> origin/chore/include-logintest
