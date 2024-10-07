db2402f386f4c73564a41e9b227124c0d28ee481
divyaka777 <divyaka77@gmail.com>
2024-10-07 14:13:53 +0200

    chore:update Resetlogin

---
+++ cypress/e2e/Tests/Reset.cy.js
@@ -0,0 +1,23 @@
+import HomePage from "../Pages/HomePage";
+import LoginPage from "../Pages/LoginPage";
+
+describe('Near Me Application', () => {
+    const homePage = new HomePage();
+
+    beforeEach(() => {
+      cy.visit('/'); // Visits the homepage
+    });
+    it('Should check if the login link exists', () => {
+        homePage.clickLoginLink();
+    });
+
+    it('Should be able to register', () => {
+        homePage.clickSignUpLink();
+    }); 
+    it('Should attempt login with valid credentials', () => {
+            homePage.clickLoginLink();
+            cy.get('input[id="email"]').clear().type('divyaka77@gmail.com');
+            cy.get('input[id="password"]').clear().type('Newyear@2020');
+            homePage.submit();
+        });
+    });
