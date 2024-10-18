331eefba6a4ec5c8039e0d824131c97be102b8d6
divyaka777 <divyaka77@gmail.com>
2024-10-18 12:37:20 +0200

    chore/update-login-backbutton-scenario

--- cypress/e2e/Pages/HomePage.js
+++ cypress/e2e/Pages/HomePage.js
@@ -1,17 +1,20 @@
 // cypress/e2e/pages/HomePage.js
 
 class HomePage {
-    visit() {
-      cy.visit('/'); // Visits the homepage
-    }
-    clickLoginLink() {
-        cy.get('a[href="/login"]').click();
-    }
-    clickSignUpLink() {
-        cy.get('a[href="/signup"]').click();
-    }
-    submit() {
-        cy.get('button[type="submit"].btn').click();
-    }
+  visit() {
+    cy.visit('/'); // Visits the homepage
+  }
+  clickLoginLink() {
+    cy.get('a[href="/login"]').click();
+  }
+  clickSignUpLink() {
+    cy.get('a[href="/signup"]').click();
+  }
+  submit() {
+    cy.get('button[type="submit"].btn').click();
+  }
+  back() {
+    cy.get('a[href="/"]').click();
+  }
 }
 export default HomePage;
